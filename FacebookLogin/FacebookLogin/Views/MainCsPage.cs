1e9147308f7fcf1ff63c89a0fbaa84bd6a8a2cbb
PonchRobles <solo-metro@hotmail.com>
2017-04-04 11:59:00 -0500

    Add project files.

---
+++ FacebookLogin/FacebookLogin/Views/MainCsPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FacebookLogin.Views
+{
+    public class MainCsPage : ContentPage
+    {
+
+        public MainCsPage()
+        {
+            Title = "Inicio de sesion con Facebook API";
+            BackgroundColor = Color.White;
+
+            var loginButton = new Button
+            {
+                Text = "Inicio de sesion con Facebook API",
+                TextColor = Color.White,
+                BackgroundColor = Color.FromHex("#01579B"),
+                Font = Font.BoldSystemFontOfSize(26),
+                FontSize = 26
+            };
+
+            loginButton.Clicked += LoginWithFacebook_Clicked;
+
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Inicio de sesion con Facebook API",
+                        FontSize = 66,
+                        TextColor = Color.Black
+                    },
+                    loginButton
+                }
+            };
+        }
+
+        private async void LoginWithFacebook_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FacebookProfileCsPage());
+        }
+    }
+}
+
+
